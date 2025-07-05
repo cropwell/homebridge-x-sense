b513ec1023f8db25a1f4ac4ce850a0c8ee81a431
Andrew Trevers <atrevers@gmail.com>
2025-07-05 12:05:42 +0100

    Refactor code structure for improved readability and maintainability

--- dist/api/constants.d.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Constants sourced from the X-Sense (Android) app and python-xsense library.
- */
-export declare const REGION = "eu-central-1";
-export declare const USER_POOL_ID = "eu-central-1_sL4n2V3j3";
-export declare const CLIENT_ID = "5sg2fo5o72pc0026unprdsdtnq";
-export declare const API_HOST = "https://api.xsense.io";
--- dist/settings.d.ts
+++
@@ -1,5 +0,0 @@
-/**
- * This is the name of the platform that users will use to register the plugin in the Homebridge config.json
- */
-export declare const PLATFORM_NAME = "X-Sense";
-export declare const PLUGIN_NAME = "homebridge-x-sense";
