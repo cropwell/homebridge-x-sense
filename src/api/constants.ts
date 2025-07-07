c144cf6a203cf33d17397f70484073b0e62f6f63
Andrew Trevers <atrevers@gmail.com>
2025-07-07 21:13:33 +0100

    chore: update version to 0.21.0 and fix IOT endpoint URL

--- src/api/constants.ts
+++ src/api/constants.ts
@@ -2,7 +2,7 @@
  * Constants sourced from the X-Sense (Android) app and python-xsense library.
  */
 export const API_HOST = 'https://api.x-sense-iot.com';
-export const IOT_ENDPOINT_URL = 'https://api.x-sense.com/v1/user/iot-endpoint';
+export const IOT_ENDPOINT_URL = 'https://api.x-sense-iot.com/v1/user/iot-endpoint';
 export const CLIENT_TYPE = '1';
 export const APP_VERSION = 'v1.22.0_20240914.1';
 export const APP_CODE = '1220';
